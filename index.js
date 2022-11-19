8d10c7deec66a93514ce46b147bf0b7ca81add1f
Lwmeek <timicivey27@gmail.com>
2022-11-18 21:49:18 -0500

    making footer-secondary for mobile screen

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const footerTwo = document.querySelector("#footer-two");
 shareBtn.forEach((share) => {
 	share.addEventListener("click", (event) => {
 		event.preventDefault();
-
-		footerOne.classList?.toggle("hide");
+		
 		console.log(footerOne.classList.contains("hide"));
 
 		footerTwo.classList?.toggle("hide");
