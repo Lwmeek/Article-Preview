1340e2620e39cd440ff3da32ecb9349a3e945e0c
Timic Ivey <timicivey27@gmail.com>
2022-12-06 09:20:34 -0500

    fixing positioning on small screen

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const shareBtn = document.querySelectorAll(".share_btn");
 const footerOne = document.querySelector("#footer-one");
 const footerTwo = document.querySelector("#footer-two");
+const btnPrimary = document.querySelector(".btn-primary");
+const btnSecondary = document.querySelector(".btn-secondary");
 
 shareBtn.forEach((share) => {
 	share.addEventListener("click", (event) => {
 		event.preventDefault();
 		
-		console.log(footerOne.classList.contains("hide"));
-
+		footerOne.classList?.toggle("hide");
 		footerTwo.classList?.toggle("hide");
+		btnSecondary.classList?.toggle("hide");
 	});
 });
+
+let btnColor = () => {
+	if(window.innerWidth >= 1050) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+console.log(btnColor());
+
+const changeColor = () => {
+	if(btnColor() === true) {
+		console.log("yes");
+		btnPrimary.style.background = "blue";
+	} else {
+		console.log("no");
+		btnPrimary.style.background = "black"
+	}
+}
+changeColor();
+window.addEventListener("resize", changeColor);
+window.addEventListener("load", changeColor);
