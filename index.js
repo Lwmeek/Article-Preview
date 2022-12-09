7fabec589e85c56d5e6184a33be4394d8d03e3cb
Timic Ivey <timicivey27@gmail.com>
2022-12-09 09:58:45 -0500

    fixed position of elements in desktop view

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-const shareBtn = document.querySelectorAll(".share_btn");
-const footerOne = document.querySelector("#footer-one");
-const footerTwo = document.querySelector("#footer-two");
-const btnPrimary = document.querySelector(".btn-primary");
-const btnSecondary = document.querySelector(".btn-secondary");
+window.addEventListener("resize", sharing);
+window.addEventListener("load", sharing);
 
-shareBtn.forEach((share) => {
-	share.addEventListener("click", (event) => {
-		event.preventDefault();
-		
-		footerOne.classList?.toggle("hide");
-		footerTwo.classList?.toggle("hide");
-		btnSecondary.classList?.toggle("hide");
+const sharing = () => {
+	const shareBtn = document.querySelectorAll(".share_btn");
+	const footerOne = document.querySelector("#footer-one");
+	const footerTwo = document.querySelector("#footer-two");
+	const btnPrimary = document.querySelector(".btn-primary");
+	const btnSecondary = document.querySelector(".btn-secondary");
+
+	shareBtn.forEach((share) => {
+		share.addEventListener("click", (event) => {
+			event.preventDefault();
+
+			if (window.innerWidth >= 1050) {
+				footerTwo.classList?.toggle("hide");
+			} else {
+				footerOne.classList?.toggle("hide");
+				footerTwo.classList?.toggle("hide");
+				btnSecondary.classList?.toggle("hide");
+			}
+		});
 	});
-});
-
-let btnColor = () => {
-	if(window.innerWidth >= 1050) {
-		return true;
-	} else {
-		return false;
-	}
 };
-
-console.log(btnColor());
-
-const changeColor = () => {
-	if(btnColor() === true) {
-		console.log("yes");
-		btnPrimary.style.background = "blue";
-	} else {
-		console.log("no");
-		btnPrimary.style.background = "black"
-	}
-}
-changeColor();
-window.addEventListener("resize", changeColor);
-window.addEventListener("load", changeColor);
