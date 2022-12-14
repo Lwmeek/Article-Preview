67b080893a665c05751ab3b201ea5ce7b0264371
Timic Ivey <timicivey27@gmail.com>
2022-12-13 19:22:50 -0500

    adjust how footer 2 will pop up after click

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-window.addEventListener("resize", sharing);
-window.addEventListener("load", sharing);
 
-const sharing = () => {
-	const shareBtn = document.querySelectorAll(".share_btn");
-	const footerOne = document.querySelector("#footer-one");
-	const footerTwo = document.querySelector("#footer-two");
-	const btnPrimary = document.querySelector(".btn-primary");
-	const btnSecondary = document.querySelector(".btn-secondary");
 
+
+
+
+
+
+
+
+	
+const shareBtn = document.querySelectorAll(".share_btn");
+const footerOne = document.querySelector("#footer-one");
+const footerTwo = document.querySelector("#footer-two");
+const btnPrimary = document.querySelector(".btn-primary");
+const btnSecondary = document.querySelector(".btn-secondary");
+	
+	
 	shareBtn.forEach((share) => {
-		share.addEventListener("click", (event) => {
-			event.preventDefault();
+	share.addEventListener("click", (event) => {
+		event.preventDefault();
 
-			if (window.innerWidth >= 1050) {
-				footerTwo.classList?.toggle("hide");
-			} else {
-				footerOne.classList?.toggle("hide");
-				footerTwo.classList?.toggle("hide");
-				btnSecondary.classList?.toggle("hide");
-			}
-		});
+		if (window.innerWidth >= 1050) {
+			footerTwo.classList?.toggle("hide");
+			btnPrimary.classList?.toggle("btn-secondary");
+		} else {
+			console.log("clicked");
+			footerTwo.style.height=("80%")
+		}
 	});
-};
+});
+
