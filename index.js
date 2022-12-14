b2c1b9d30abb0b0327580865a695d35bc7d70427
Timic Ivey <timicivey27@gmail.com>
2022-12-14 08:50:20 -0500

    fixed up hide show feature

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-
-
-
-
-
-
-
-
-
-	
 const shareBtn = document.querySelectorAll(".share_btn");
 const footerOne = document.querySelector("#footer-one");
 const footerTwo = document.querySelector("#footer-two");
 const btnPrimary = document.querySelector(".btn-primary");
 const btnSecondary = document.querySelector(".btn-secondary");
-	
-	
-	shareBtn.forEach((share) => {
+
+shareBtn.forEach((share) => {
 	share.addEventListener("click", (event) => {
 		event.preventDefault();
 
 		if (window.innerWidth >= 1050) {
 			footerTwo.classList?.toggle("hide");
+			footerTwo.classList?.toggle("show");
 			btnPrimary.classList?.toggle("btn-secondary");
 		} else {
 			console.log("clicked");
-			footerTwo.style.height=("80%")
+			footerTwo.classList?.toggle("show");
+			footerTwo.classList?.toggle("hide");
 		}
 	});
 });
-
